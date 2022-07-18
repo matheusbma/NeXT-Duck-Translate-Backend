9bf5f002a3e5cf52700bc082d507e469bda552b2
Matheus Araújo <mbma.dev@gmail.com>
2022-07-18 18:20:25 -0300

    Duck Translate 1.0

--- __main__.py
+++ __main__.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_cors import CORS
 from dotenv import load_dotenv
@@ -21,5 +20,4 @@ initializeSpeechToText(app)
 def index():
     return "index"
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
+app.run(debug=True)
